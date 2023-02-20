66dab1dbe86d5bc5961d5db236a0e52f7493f0e1
Ssuamje <side2850@naver.com>
2023-02-20 14:53:18 +0900

    modify download.sh

--- download.sh
+++ download.sh
@@ -1,13 +1,14 @@
 GIT="Ssuamje/my_git_tools.git"
 REPO="my_git_tools"
 FILE="fast_commit_push.sh"
+FILE_HIDDEN=".fast_commit_push.sh"
 MY_ALIAS="gfc"
 
 git clone git@github.com:$GIT
 
 #make file hidden
 mv $REPO/$FILE $HOME
-mv $HOME/$FILE $HOME/.$FILE
+mv $HOME/$FILE $HOME/$FILE_HIDDEN
 rm -rf $REPO
 
 SRC="$HOME/.zshrc"
@@ -19,9 +20,9 @@ if [ "$(uname)" != "Darwin" ]; then
 	fi
 fi
 
-if [ $(cat $SRC | grep "$FILE" | wc -l) -eq 0 ] 
+if [ $(cat $SRC | grep "$FILE" | wc -l) -eq 0 ]
 then
-	echo -e "\nalias $MY_ALIAS=\"bash $HOME/.$FILE" >> "$SRC"
+	echo -e "\nalias $MY_ALIAS=\"bash $HOME/$FILE_HIDDEN\"" >> "$SRC"
 fi
 
 exec "$SHELL"
