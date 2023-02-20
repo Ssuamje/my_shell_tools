2b0c520f02ab9961f17ad4ecdaa4dc008ac07b89
Ssuamje <side2850@naver.com>
2023-02-20 12:11:30 +0900

    arg

--- fast_commit_push.sh
+++ fast_commit_push.sh
@@ -41,76 +41,76 @@ if $quiet; then
 	echo -e -n $YELLOW"Push : "
 	git push $(git remote) $(git branch | grep \* | awk '{ print $2 }')
 	exit 0
-fi
-
-#check git status
-clear
-while true; do
-	added=$(git status --porcelain | grep -E '(^ A|^A)' | cut -c 4-)
-	modified=$(git status --porcelain | grep -E '(^ M|^M)' | cut -c 4-)
-	untracked=$(git status --porcelain | grep -E '^\?\?' | cut -c 4-)
-	echo -e $GREEN"@___Added___@"
-	for file in $added; do
-		echo "$file"
-	done
-	echo -e "@-----------@\n" $RESET
-	echo -e $RED"@___Modified___@"
-	for file in $modified; do
-		echo "$file"
-	done
-	echo -e "@--------------@\n" $RESET
-	echo -e $RED"@___Untracked___@"
-	for file in $untracked; do
-		echo "$file"
-	done
-	echo -e "@---------------@\n" $RESET
-
-	echo -e $CYAN"Want to update all changes to current branch? (y/n)"$YELLOW
-	read answer
-
-	answer="$(echo "${answer}" | tr '[:upper:]' '[:lower:]')"
-	if [ "$answer" == "y" ]; then
-		break
-	elif [ "$answer" == "n" ]; then
-		echo -e $CYAN"Exiting..."
-		exit 0
-	else
-		echo $CYAN"Invalid input. Please enter 'y' or 'n'." $RESET
-	fi
-done
-
-#move to git root directory
-cd $(git rev-parse --show-toplevel)
-
-#add all changes
-git add .
-cd -
-
-#check commit message
-message=$1
-while true; do
+else
+	#check git status
 	clear
-	echo -e $YELLOW"Message : \"$message\""
-	echo -e $CYAN"is it right? (y/n)"$YELLOW
-	read answer
+	while true; do
+		added=$(git status --porcelain | grep -E '(^ A|^A)' | cut -c 4-)
+		modified=$(git status --porcelain | grep -E '(^ M|^M)' | cut -c 4-)
+		untracked=$(git status --porcelain | grep -E '^\?\?' | cut -c 4-)
+		echo -e $GREEN"@___Added___@"
+		for file in $added; do
+			echo "$file"
+		done
+		echo -e "@-----------@\n" $RESET
+		echo -e $RED"@___Modified___@"
+		for file in $modified; do
+			echo "$file"
+		done
+		echo -e "@--------------@\n" $RESET
+		echo -e $RED"@___Untracked___@"
+		for file in $untracked; do
+			echo "$file"
+		done
+		echo -e "@---------------@\n" $RESET
 
-	answer="$(echo "${answer}" | tr '[:upper:]' '[:lower:]')"
-	if [ "$answer" == "y" ]; then
+		echo -e $CYAN"Want to update all changes to current branch? (y/n)"$YELLOW
+		read answer
+
+		answer="$(echo "${answer}" | tr '[:upper:]' '[:lower:]')"
+		if [ "$answer" == "y" ]; then
+			break
+		elif [ "$answer" == "n" ]; then
+			echo -e $CYAN"Exiting..."
+			exit 0
+		else
+			echo $CYAN"Invalid input. Please enter 'y' or 'n'." $RESET
+		fi
+	done
+
+	#move to git root directory
+	cd $(git rev-parse --show-toplevel)
+
+	#add all changes
+	git add .
+	cd -
+
+	#check commit message
+	message=$1
+	while true; do
 		clear
-		break
-	elif [ "$answer" == "n" ]; then
-		clear
-		echo -e $CYAN"Input commit message you want to change."$YELLOW
-		read message
-	else
-		echo "Invalid input. please enter y or n."
-	fi
-done
+		echo -e $YELLOW"Message : \"$message\""
+		echo -e $CYAN"is it right? (y/n)"$YELLOW
+		read answer
 
-#set commit message
-echo -e -n $YELLOW"Commit : "
-git commit -m "$message" | sed -n '2p'
+		answer="$(echo "${answer}" | tr '[:upper:]' '[:lower:]')"
+		if [ "$answer" == "y" ]; then
+			clear
+			break
+		elif [ "$answer" == "n" ]; then
+			clear
+			echo -e $CYAN"Input commit message you want to change."$YELLOW
+			read message
+		else
+			echo "Invalid input. please enter y or n."
+		fi
+	done
 
-#get current working branch
-echo -e -n $YELLOW"Push : "
-git push $(git remote) $(git branch | grep \* | awk '{ print $2 }')
+	#set commit message
+	echo -e -n $YELLOW"Commit : "
+	git commit -m "$message" | sed -n '2p'
+
+	#get current working branch
+	echo -e -n $YELLOW"Push : "
+	git push $(git remote) $(git branch | grep \* | awk '{ print $2 }')
+fi
