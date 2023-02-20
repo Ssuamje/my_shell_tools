ed9f2ffec1b9c295499a1d2b844ac273cdf34221
Ssuamje <side2850@naver.com>
2023-02-20 21:35:31 +0900

    added https download

--- download.sh
+++ download.sh
@@ -1,10 +1,11 @@
 GIT="Ssuamje/my_git_tools.git"
+GIT_HTTPS=https://github.com/Ssuamje/my_git_tools.git
 REPO="my_git_tools"
 FILE="fast_commit_push.sh"
 FILE_HIDDEN=".fast_commit_push.sh"
 MY_ALIAS="gfc"
 
-git clone git@github.com:$GIT
+git clone git@github.com:$GIT || git clone git@github.com:$GIT_HTTPS
 
 #make file hidden
 mv $REPO/$FILE $HOME
